25e7a80842a541229dbc6bf681c2af68888ca516
Тарас Леоновець <trs.leonov@gmail.com>
2021-12-18 12:35:10 +0200

    Printer: поправил переменные и добавил метод getPendingPagesCount

--- 03_ObjectsAndMethods/src/Printer.java
+++ 03_ObjectsAndMethods/src/Printer.java
@@ -1,8 +1,22 @@
 public class Printer {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.append("The new version adds processing with data", "Report", 13);
+        printer.clear();
+        printer.append("Это тестовый документ");
+        printer.append("I give permission for the processing of my data", 12);
+
+        System.out.println("Pending pages count: " + printer.getPendingPagesCount() + "\n");
+        printer.print();
+        System.out.println("Pending pages count: " + printer.getPendingPagesCount() + "\n");
+        System.out.println("Printed pages count: " + printer.getPrintedPages() + "\n");
+
+    }
+
     private String queue = "";
-    private int countPage = 0;
-    static int countPrintedPages = 0;
-    private String namesDocuments = "";
+    private int countPrintedPages = 0;
+    private String namesOfDocuments = "";
+    private int countPendingPages = 0;
 
     public void append(String text){
         append(text, "noname", 1);
@@ -14,25 +28,27 @@ public class Printer {
 
     public void append(String text, String name, int countPage){
         queue +=text + "\n";
-        this.countPage += countPage;
-        namesDocuments += name;
+        this.countPendingPages += countPage;
+        namesOfDocuments += " " + name;
     }
 
     public void clear(){
         queue = "";
-        countPage = 0;
-        namesDocuments = "";
+        countPendingPages = 0;
+        namesOfDocuments = "";
     }
 
     public void print(){
         System.out.print("Text of document: " + queue);
-        System.out.print("Name of document: " + namesDocuments + "\n");
-        System.out.println("Number of pages = " + countPage + "\n");
+        System.out.print("Name of document: " + namesOfDocuments + "\n");
+        System.out.println("Number of pages = " + countPendingPages + "\n");
         queue = "";
-        namesDocuments = "";
-        countPrintedPages += countPage;
-        countPage = 0;
+        namesOfDocuments = "";
+        countPrintedPages += countPendingPages;
+        countPendingPages = 0;
     }
 
-    public static int getPrintedPages(){ return countPrintedPages;}
+    public int getPendingPagesCount() {return countPendingPages;}
+
+    public int getPrintedPages(){ return countPrintedPages;}
 }
