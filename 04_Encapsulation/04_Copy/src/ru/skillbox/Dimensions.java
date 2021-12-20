3cbe4d7291fee3d4ad010e8d819954a18a96e785
Тарас Леоновець <trs.leonov@gmail.com>
2021-12-20 23:14:08 +0200

    Я выполнил работу 5.4. Но не уверен, что нужно использовать 3 копии

---
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Dimensions.java
@@ -0,0 +1,17 @@
+package ru.skillbox;
+
+public class Dimensions {
+    private final int width; // at centimeter
+    private final int height; // at centimeter
+    private final int length; // at centimeter
+
+    public Dimensions(int width, int height, int length) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+    public double getVolume(){
+        return (width * height * length);
+    }
+}
--- 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
@@ -1,7 +1,24 @@
 package ru.skillbox;
 
 public class Main {
-
     public static void main(String[] args) {
+        InfoAtCargoes parcel = new InfoAtCargoes(12, 2, 3, 12.2,
+                "Pushkinska 25 A", true,
+                "AS44322", true
+        );
+
+        InfoAtCargoes copy1 = parcel.setAddress("Yangelya 2");
+        InfoAtCargoes copy2 = copy1.setDimensions(2,3,4);
+        InfoAtCargoes copy = copy2.setMass(0.24);
+
+        System.out.println("Information about your parcel" + "\n" + "Dimensions: " + copy.getWidth() + "*" +
+                copy.getHeight() + "*" +  copy.getLength() + "\n" +
+                "Mass: " + copy.getMass() + " kg" + "\n" + "Address: " + copy.getAddress() + "\n" +
+                (copy.isCanTurn() ? "Can be turned over on delivery" : "Don't can be turned over on delivery") + "\n"
+                + "Number of registration: " + copy.getNumberOfRegistration() + "\n" +
+                (copy.isFragile() ? "Fragile" : "Not fragile") + "\n");
+
+        Dimensions volume = new Dimensions(copy.getWidth(), copy.getHeight(), copy.getLength());
+        System.out.println("Volume - " + volume.getVolume() + "*10^3" + " litres");
     }
 }
