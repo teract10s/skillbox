18384966866430c3ada21885307fe89ab23cd410
Тарас Леоновець <trs.leonov@gmail.com>
2021-12-21 17:35:17 +0200

    Я выполнил работу 6.1

--- 03_ObjectsAndMethods/src/Basket.java
+++ 03_ObjectsAndMethods/src/Basket.java
@@ -3,7 +3,7 @@ public class Basket {
     private static int countProduct = 0;
     private static int countBasket = 0;
     private static int totalPriceAtAllBasket;
-    private String items = "";
+    private String items;
     private int totalPrice = 0;
     private int limit;
     private double totalWeight = 0;
@@ -110,5 +110,18 @@ public class Basket {
         } else {
             System.out.println(items);
         }
+    }
+
+    public static void main(String[] args) {
+        Basket basket1 = new Basket();
+        basket1.add("Milk", 40);
+        basket1.add("Bear", 50);
+
+        Basket basket2 = new Basket();
+        basket2.add("Napkins", 10);
+        basket2.add("Watter", 20);
+
+        System.out.println("Average price at product: " + Basket.averagePriceProduct() + "\n" +
+                "Average price at basket: " + Basket.averagePriceBasket() + "\n");
     }
 }
