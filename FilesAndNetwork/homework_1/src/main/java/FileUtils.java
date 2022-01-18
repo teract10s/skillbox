0380f94771c7d187bbdb1a9d3f3ec73609880360
Тарас Леоновець <trs.leonov@gmail.com>
2022-01-19 00:19:59 +0200

    Done

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,6 +1,20 @@
+import java.io.File;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        try {
+            File folder = new File(path);
+            File[] files = folder.listFiles();
+            long sum = 0;
+
+            for (File file : files) {
+                sum += file.isDirectory() ? FileUtils.calculateFolderSize(file.toString()) : file.length();
+            }
+            return sum;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 }
--- FilesAndNetwork/homework_1/src/main/java/Main.java
+++ FilesAndNetwork/homework_1/src/main/java/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь к папке");
+        String path = scanner.next();
 
+        String answer = "Размер папки " + path + " cоставляет "
+                + trueDimensionality(FileUtils.calculateFolderSize(path));
+        System.out.println(answer);
+    }
+
+    private static String trueDimensionality(double size){
+        String answer = "";
+        int count = 0;
+        while (size > 1024){
+            size /= 1024;
+            count++;
+        }
+        answer += size;
+
+        switch (count){
+            case 0 -> answer += " б";
+            case 1 -> answer += " Кб";
+            case 2 -> answer += " Мб";
+            case 3 -> answer += " Гб";
+        }
+        return answer;
     }
 }
