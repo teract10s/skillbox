032e03818b8220d8fe52a1ab38ae82b009e46a53
Тарас Леоновець <trs.leonov@gmail.com>
2022-01-19 10:54:00 +0200

    Done with Files.walkFileTree

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,20 +1,45 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileUtils {
+    private static long sizeOfDirectory;
 
     public static long calculateFolderSize(String path) {
         try {
             File folder = new File(path);
-            File[] files = folder.listFiles();
-            long sum = 0;
 
-            for (File file : files) {
-                sum += file.isDirectory() ? FileUtils.calculateFolderSize(file.toString()) : file.length();
-            }
-            return sum;
+            FileVisitor<? super Path> visitor = new FileVisitor<>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                    sizeOfDirectory += file.toFile().length();
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+                    return FileVisitResult.CONTINUE;
+                }
+            };
+
+            Files.walkFileTree(folder.toPath(),  visitor);
+
+            return sizeOfDirectory;
         }catch (Exception e){
             System.out.println(e.getMessage());
             return 0;
         }
     }
+
 }
