92583dc9b45bad0b8c08c5d6339cd75eaede98e9
Тарас Леоновець <trs.leonov@gmail.com>
2022-02-10 18:37:45 +0200

    'add_hibernate'

---
+++ SQLAndHibernate/hibernate/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        Session session = sessionFactory.openSession();
+
+        for (int i = 1; i < 46; i++){
+            Course course = session.get(Course.class, i);
+            System.out.println(course.getName() + "\t" + course.getStudentsCount());
+        }
+
+        sessionFactory.close();
+    }
+}
