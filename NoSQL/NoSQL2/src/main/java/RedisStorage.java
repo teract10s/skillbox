7b9bb5193a31895362286e1304d245c86e9113b0
Тарас Леоновець <trs.leonov@gmail.com>
2022-04-10 13:17:35 +0300

    'first_commit_HW2.'

--- NoSQL/NoSQL2/src/main/java/RedisStorage.java
+++ NoSQL/NoSQL2/src/main/java/RedisStorage.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class RedisStorage {
+import org.redisson.Redisson;
+import org.redisson.api.RKeys;
+import org.redisson.api.RScoredSortedSet;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.RedisConnectionException;
+import org.redisson.config.Config;
+
+import static java.lang.System.out;
+
+public class RedisStorage {
+    private RedissonClient redisson;
+    private RScoredSortedSet<String> onlineUsers;
+    private RKeys rKeys;
+    private final static String KEY = "USERS";
+
+    void init() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        try {
+            redisson = Redisson.create(config);
+        } catch (RedisConnectionException Exc) {
+            out.println("Не удалось подключиться к Redis");
+            out.println(Exc.getMessage());
+        }
+        rKeys = redisson.getKeys();
+        onlineUsers = redisson.getScoredSortedSet(KEY);
+        rKeys.delete(KEY);
+    }
+
+    void shutdown() {
+        redisson.shutdown();
+    }
+
+    void add(int numberInTheQueue, int userId) {
+        onlineUsers.add(numberInTheQueue, String.valueOf(userId));
+    }
+
+    void delete(int numberInTheQueue) {
+        onlineUsers.remove(numberInTheQueue);
+    }
+
+    public RScoredSortedSet<String> getOnlineUsers() {
+        return onlineUsers;
+    }
+
+    public String getFirstUserInQueue(){
+        return onlineUsers.first();
+    }
 }
--- NoSQL/NoSQL2/src/main/java/SiteWork.java
+++ NoSQL/NoSQL2/src/main/java/SiteWork.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class SiteWork {
+import java.util.Random;
+
+public class SiteWork {
+    private static final int USERS = 20;
+    private static final int SLEEP = 1000;
+    private static RedisStorage redis;
+
+    public static void main(String[] args) throws InterruptedException {
+        redis = new RedisStorage();
+        redis.init();
+        init();
+
+        int i = 0;
+        for (;;){
+            i++;
+            for(int j = 0; j < USERS; j++) {
+                if (i == 10) {
+                    i = 0;
+                    int userId = new Random().nextInt(20);
+                    redis.delete(userId);
+                    redis.add(1, userId);
+                }
+                String firstUserAtQueue = redis.getFirstUserInQueue();
+                System.out.println("На главной странице показываем пользователя " + firstUserAtQueue);
+                redis.delete(Integer.parseInt(firstUserAtQueue));
+                redis.add(20, Integer.parseInt(firstUserAtQueue));
+            }
+            Thread.sleep(SLEEP);
+        }
+    }
+
+    private static void init(){
+        for (int j = 0; j < USERS; j++){
+            redis.add(j, j);
+        }
+    }
 }
